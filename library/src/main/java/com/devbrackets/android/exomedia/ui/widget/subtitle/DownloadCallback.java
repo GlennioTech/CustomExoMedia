23d0023db8e23e9e9a0a8757a0cbf8244023e1eb
surahul <denhelp1@gmail.com>
2018-04-03 20:47:26 +0530

    final subs scene complete

--- library/src/main/java/com/devbrackets/android/exomedia/ui/widget/subtitle/DownloadCallback.java
+++
@@ -1,8 +0,0 @@
-package com.devbrackets.android.exomedia.ui.widget.subtitle;
-
-import java.io.File;
-
-public interface DownloadCallback {
-    public void onDownload(File file);
-    public void onFail(Exception e);
-}
