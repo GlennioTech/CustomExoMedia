aec95cc78903c46f33bd9aca821bfba74f1a12e1
surahul <denhelp1@gmail.com>
2019-06-03 16:51:11 +0530

    Merging sources if audio sourse for hls and dash media sources

--- library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/DashMediaSourceBuilder.java
+++ library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/DashMediaSourceBuilder.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
 
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -21,7 +22,12 @@ public class DashMediaSourceBuilder extends MediaSourceBuilder {
     protected MediaSource buildInternal(@NonNull Context context, @NonNull Uri uri, @Nullable Uri audioUri, @Nullable List<Pair<String, String>> headers, @NonNull String userAgent, @NonNull Handler handler, @Nullable TransferListener<? super DataSource> transferListener) {
         DataSource.Factory dataSourceFactory = buildDataSourceFactory(context, headers, userAgent, null);
         DataSource.Factory meteredDataSourceFactory = buildDataSourceFactory(context, headers, userAgent, transferListener);
-
+        if(audioUri!=null){
+            return new MergingMediaSource(new DashMediaSource.Factory(new DefaultDashChunkSource.Factory(meteredDataSourceFactory), dataSourceFactory)
+                    .createMediaSource(uri, handler, null),
+                    new DashMediaSource.Factory(new DefaultDashChunkSource.Factory(meteredDataSourceFactory), dataSourceFactory)
+                            .createMediaSource(audioUri, handler, null));
+        }
         return new DashMediaSource.Factory(new DefaultDashChunkSource.Factory(meteredDataSourceFactory), dataSourceFactory)
                 .createMediaSource(uri, handler, null);
     }
--- library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/HlsMediaSourceBuilder.java
+++ library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/HlsMediaSourceBuilder.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
 
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.TransferListener;
@@ -19,6 +20,12 @@ public class HlsMediaSourceBuilder extends MediaSourceBuilder {
     @Override
     protected MediaSource buildInternal(@NonNull Context context, @NonNull Uri uri, @Nullable Uri audioUri, @Nullable List<Pair<String, String>> headers, @NonNull String userAgent, @NonNull Handler handler, @Nullable TransferListener<? super DataSource> transferListener) {
         DataSource.Factory dataSourceFactory = buildDataSourceFactory(context, headers, userAgent, transferListener);
+        if(audioUri!=null){
+            return new MergingMediaSource(new HlsMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(uri, handler, null),
+                    new HlsMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(audioUri, handler, null));
+        }
         return new HlsMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(uri, handler, null);
     }
